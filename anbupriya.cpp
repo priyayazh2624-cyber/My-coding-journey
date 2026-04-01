45d7c4485132e042199d41b4b73c98576507f5ef
priyayazh2624-cyber <priyayazh2624@gmail.com>
2026-04-01 12:49:20 +0530

    Add files via upload

---
+++ anbupriya.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,add;
+	printf("enter the first no:");
+	scanf("%d",&num1);
+	printf("enter the second no:");
+	scanf("%d",&num2);
+	add=num1+num2;
+	printf("addition=%d",add);
+	sub=num1-num;
+	printf("subraction=%d",sub);
+	mult=num1*num2;
+	printf("multiplication=%d",mult);
+	div=num1/num2;
+	printf("division=%d",div);
+	return 0;
+}
